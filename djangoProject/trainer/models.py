3e3c77f7f556b91f4fa1b3b1061d6a8b2a253f07
mykyyta <nikita.glushko@gmail.com>
2024-12-12 21:34:41 +0200

    models

--- djangoProject/trainer/models.py
+++ djangoProject/trainer/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class TrainerDescription(models.Model):
+    name = models.TextField()
+    trainer = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class TrainerSchedule(models.Model):
+    datetime_start = models.DateTimeField()
+    datetime_end = models.DateTimeField()
+    trainer = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+class Service(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    trainer = models.ForeignKey(User, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    level = models.CharField(max_length=50)
+    duration = models.CharField(max_length=50)
+
+
+
