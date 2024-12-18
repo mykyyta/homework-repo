e1bede5cfb4ee4e65ceada76f41824cd352bad76
mykyyta <nikita.glushko@gmail.com>
2024-12-18 20:55:10 +0200

    django project removed after moving to new repo

--- djangoProject/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.user_page, name='user_page'),
-    path('<int:user_id>/', views.specific_user, name='specific_user'),
-]
